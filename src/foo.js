4b3b2d17466e5b738624ceb6a375ce8ea0716520
Andrey Isenko <aisenko87@gmail.com>
2025-12-22 21:41:36 +0200

    add create

--- src/foo.js
+++ src/foo.js
@@ -1,4 +1,4 @@
-export function foo(tasks) {
+function foo(tasks) {
   return `<ul class="list">${tasks
     .map(
       task =>
@@ -6,3 +6,39 @@ export function foo(tasks) {
     )
     .join('')}</ul>`;
 }
+//  console.log('fot');
+
+export const tot = 10;
+export const sum = 55;
+export default foo;
+function goo(params) {
+  // console.log('goo');
+}
+goo();
+
+const rrr = () => {
+  // console.log('JR');
+};
+// rrr();
+setTimeout(rrr, 2000);
+// console.log(Date());
+// console.log(Date.now());
+// const data = new Date();
+// console.log(data);
+// console.log(data.getDay());
+// console.log(data.getHours());
+// console.log(data.getMinutes());
+// console.log(data.getDate());
+// console.log(data.getMonth());
+// console.log(data);
+const date1 = Date.now();
+// console.log(date1);
+// const timerId = setTimeout(() => {
+//   const time2 = Date.now();
+//   console.log(time2 - date1);
+// }, 2000);
+
+// const intervalID = setInterval(() => {
+//   const time3 = Date.now();
+//   console.log(time3 - date1);
+// }, 2000);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import './style.css';
 import { tasks } from '../task';
-import { foo } from './foo';
+import foo from './foo';
+// import { tot } from './foo';
+import * as modF from './foo';
+// console.log(modF);
+// console.log(modF.tot);
+// console.log(modF.sum);
+
 const formA = document.querySelector('#task');
 const wrap = document.querySelector('#app');
+const wraps = document.querySelector('#appi');
+console.log(wraps);
+
 formA.addEventListener('submit', handleSubmit);
 const searchInp = document.querySelector('.searchTask');
 
@@ -29,5 +38,50 @@ wrap.innerHTML = foo(tasks);
 const taskList = document.querySelector('.list');
 taskList.addEventListener('click', handleDeleteTask);
 function handleDeleteTask(event) {
-  console.log(event.target);
+  if (event.currentTarget.children) {
+  }
+  // console.log(Array.isArray(event.currentTarget.children));
+  // console.log(event.currentTarget.children.length);
 }
+
+let Tasks = ['eat', 'sleep', 'code'];
+const ttt = Tasks.splice(1, 1, 'ss'); // видаляє 1 елемент з індексу 1
+// console.log(ttt); // ["eat", "code"]
+// console.log(Tasks); // ["eat", "code"]
+
+Tasks.splice(1, 0, 'walk'); // додає "walk" на індекс 1
+// console.log(Tasks); // ["eat", "walk", "code"]
+console.log(55);
+// const url = 'https://drive.usercontent.google.com';
+// fetch(url)
+//   .then(resp => {
+//     if (!resp.ok) {
+//       throw new Error(resp.status);
+//     }
+//     return resp.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// const photo = document.createElement('img');
+// photo.classList.add('imago');
+// photo.src =
+//   'https://drive.usercontent.google.com/uc?id=1mEfP9LUDu7VbcsyNj2HlzBs5ZeyMFZUE';
+
+// photo.alt = 'YANA';
+// wrap.prepend(photo);
+
+const photo = document.createElement('img');
+photo.classList.add('imago');
+
+// photo.src = 'https://drive.google.com/uc?id=1mEfP9LUDu7VbcsyNj2HlzBs5ZeyMFZUE';
+// photo.src = 'https://picsum.photos/400';
+photo.src = './src/photo/KETI9910.jpg';
+
+photo.alt = 'YANA';
+
+wraps.append(photo);
